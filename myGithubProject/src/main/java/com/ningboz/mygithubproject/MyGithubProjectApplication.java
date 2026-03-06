aff028d5ee9520acfa2bcbfbb1e22afe0b653459
zhangningbo <ningbo.zhang@bangtu-ai.cn>
2026-03-06 10:10:27 +0800

    初始化项目

---
+++ myGithubProject/src/main/java/com/ningboz/mygithubproject/MyGithubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ningboz.mygithubproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGithubProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGithubProjectApplication.class, args);
+    }
+
+}
---
+++ myGithubProject/src/test/java/com/ningboz/mygithubproject/MyGithubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ningboz.mygithubproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGithubProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
