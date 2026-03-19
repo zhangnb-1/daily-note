c1081655ae743eb6379234b7663144fb2f8d174d
zhangnb <851306324@qq.com>
2026-03-19 23:06:12 +0800

    数据库文档整理

---
+++ myGithubProject/src/main/java/com/ningboz/mygithubproject/test/Test.java
@@ -0,0 +1,11 @@
+package com.ningboz.mygithubproject.test;
+
+public class Test {
+    public void test(){
+        int num = 20;
+        User zs = new User("1", "zs");
+        System.out.println(num);
+        System.out.println(zs.getId());
+        System.out.println(zs.getName());
+    }
+}
