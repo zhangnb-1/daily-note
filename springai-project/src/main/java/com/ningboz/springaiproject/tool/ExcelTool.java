20a01187d55b280eb40d0d43b3ee9619b45a0247
zhangnb <851306324@qq.com>
2026-03-30 23:23:01 +0800

    ai agent

---
+++ springai-project/src/main/java/com/ningboz/springaiproject/tool/ExcelTool.java
@@ -0,0 +1,7 @@
+package com.ningboz.springaiproject.tool;
+
+public interface ExcelTool {
+    void readFile(String path);
+    void act2();
+    void act3();
+}
---
+++ springai-project/src/main/java/com/ningboz/springaiproject/tool/PageTool.java
@@ -0,0 +1,5 @@
+package com.ningboz.springaiproject.tool;
+
+public class PageTool {
+    // 前端页面 agent
+}
