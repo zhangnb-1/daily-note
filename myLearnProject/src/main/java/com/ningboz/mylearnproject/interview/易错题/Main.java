1ac3d010b28daf1867338968c4b076f3d3a0a507
zhangningbo <ningbo.zhang@bangtu-ai.cn>
2026-04-13 18:08:34 +0800

    26/03 新加项目

--- myLearnProject/src/main/java/com/ningboz/mylearnproject/interview/易错题/Main.java
+++ myLearnProject/src/main/java/com/ningboz/mylearnproject/interview/易错题/Main.java
@@ -1,10 +1,13 @@
 package com.ningboz.mylearnproject.interview.易错题;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(finallyShunxu());
+//        System.out.println(finallyShunxu());
+        threadStateMethod();
     }
 
     // finally 代码块执行顺序
@@ -24,8 +27,36 @@ public class Main {
     private static void futureException(){
         CompletableFuture future = new CompletableFuture<>();
 
+
     }
+
+    public Main() {
+        super();
+    }
+
     // 消息摘要算法
     // 线程状态 以及 转换
+    private static void threadStateMethod(){
+        /**
+         *  传统线程状态：初始，就绪，运行，阻塞，终止
+         *  java线程状态：初始，可运行，阻塞，限时等待，不限时等待，终止
+         *  对应：
+         *      1. 就绪，运行 -> 可运行
+         *      2. 阻塞 -> 阻塞、限时等待、不限时等待
+         *
+         */
+        Object lock = new Object();
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                System.out.println("execute1 thread");;
+            }
+        };
+        System.out.println(thread.getState());
+        ReentrantLock lock1 = new ReentrantLock();
+        Condition condition = lock1.newCondition();
+//        condition.await();
+
+    }
     // redis数据类型
 }
